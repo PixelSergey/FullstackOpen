e1ee602263bda90bb4c281104d6a3a7eadae331f
PixelSergey <sergey.ichtchenko@gmail.com>
2025-03-03 01:58:59 +0000

    Solve problem 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -16,6 +16,20 @@ const Stat = ({label, value}) => {
   )
 }
 
+const Statistics = ({values}) => {
+  return (
+    <>
+      <h1>Statistics</h1>
+      <Stat label={"good"} value={values[0]} />
+      <Stat label={"neutral"} value={values[1]} />
+      <Stat label={"bad"} value={values[2]} />
+      <Stat label={"all"} value={values.reduce((partialSum, a) => partialSum + a, 0)} />
+      <Stat label={"average"} value={(values[0]-values[2])/(values[0]+values[1]+values[2])} />
+      <Stat label={"positive"} value={values[0]/(values[0]+values[1]+values[2])} />
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -28,10 +42,7 @@ const App = () => {
       <Button label={"good"} value={good} setter={setGood} />
       <Button label={"neutral"} value={neutral} setter={setNeutral} />
       <Button label={"bad"} value={bad} setter={setBad} />
-      <h1>Statistics</h1>
-      <Stat label={"good"} value={good} />
-      <Stat label={"neutral"} value={neutral} />
-      <Stat label={"bad"} value={bad} />
+      <Statistics values={[good, neutral, bad]} />
       </>
   )
 }
